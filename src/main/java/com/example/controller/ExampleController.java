f78d9550f795ff56c1c6b515a1174aecbe7aeddc
VOLOVITSKIY VALERIY <71818080+cmunknown@users.noreply.github.com>
2022-08-17 10:57:24 +0300

    Update ExampleController.java

--- src/main/java/com/example/controller/ExampleController.java
+++ src/main/java/com/example/controller/ExampleController.java
@@ -15,6 +15,6 @@ public class ExampleController {
     
     @GetMapping
     public String getServiceName() {
-        return exampleService.getSelfName() + " hello from new version";
+        return exampleService.getSelfName() + " hello from new mmmmmmmmmm";
     }
 }
